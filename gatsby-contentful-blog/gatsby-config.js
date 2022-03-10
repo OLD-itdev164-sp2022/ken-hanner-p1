d7938a1da10c501bff0df464966818502b7b2296
Ken <ken.hanner@gmail.com>
2022-03-09 22:05:26 -0600

    create page/blogposts.js, reset metadata

--- gatsby-contentful-blog/gatsby-config.js
+++ gatsby-contentful-blog/gatsby-config.js
@@ -1,10 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
-  },
+title: `My Project 1 Blog`,
+description: `A Project 1 blog displaying Project 1 posts.`,
+author: `Ken Hanner`,
+},
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
--- gatsby-contentful-blog/src/pages/index.js
+++ gatsby-contentful-blog/src/pages/index.js
@@ -1,31 +1,24 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import React from "react";
+import { Link } from "gatsby";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Image from "../components/image";
+import SEO from "../components/seo";
+import "./index.css";
 
 const IndexPage = () => (
   <Layout>
-    <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p>
-  </Layout>
-)
+  <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
+  <div className='home'>
+  <h1>Hello There</h1>
+  <p>Welcome my awesome blog</p>
+  <div>
+  <div          style={{            maxWidth: `300px`,            margin: '0 auto 1.45rem',          }}        >
+  <Image />
+  </div>
+  </div>
+  <Link to='/blogposts/'>View all posts</Link>
+  </div>
+  </Layout>);
 
-export default IndexPage
+export default IndexPage;
